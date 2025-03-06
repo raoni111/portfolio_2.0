916ac4252866e1db6249fd5261b6ee380b1d5edf
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2025-03-06 09:50:42 -0300

    update-header-responsiveness

--- src/components/header-element.vue
+++ src/components/header-element.vue
@@ -5,60 +5,121 @@ import LinkedinIcon from './icons/header/linkedin-icon.vue'
 
 <template>
   <header class="header-element">
-    <h1 class="title">{{ "<RQ />" }}</h1>
-    <ul class="header-social-list">
-      <li class="social-icon">
-        <a href="https://www.linkedin.com/in/raoni-oliveira-quevedo-3719871b3" target="_blank">
-          <LinkedinIcon />
-          <h2 class="social-name">Linkedin</h2>
-        </a>
-      </li>
-      <li class="social-icon">
-        <a href="https://github.com/raoni111" target="_blank">
-          <GithubIcon />
-          <h2 class="social-name">Github</h2>
-        </a>
-      </li>
-    </ul>
+    <div class="header-content">
+      <h1 class="title">{{ "<RQ />" }}</h1>
+      <ul class="header-social-list">
+        <li class="social-icon">
+          <a href="https://www.linkedin.com/in/raoni-oliveira-quevedo-3719871b3" target="_blank">
+            <LinkedinIcon />
+            <h2 class="social-name">Linkedin</h2>
+          </a>
+        </li>
+        <li class="social-icon">
+          <a href="https://github.com/raoni111" target="_blank">
+            <GithubIcon />
+            <h2 class="social-name">Github</h2>
+          </a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .header-element {
-  position: fixed;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  position: fixed;
   width: 100dvw;
-  padding: 1.5rem 6rem;
-
-  .title {
-    color: var(--font-color-green);
-    font-size: 1.8rem;
-  }
+  .header-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 75%;
+    padding: 1rem 0rem;
+    .title {
+      color: var(--font-color-green);
+      font-size: 1.8rem;
+    }
 
     .header-social-list {
       display: flex;
       list-style: none;
 
-    .social-icon {
-
-      a {
-        cursor: pointer;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        color: var(--font-color-green);
+      .social-icon {
         &:nth-child(1) {
-          margin-right: 2rem;
+            margin-right: 1rem;
         }
+        a {
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: var(--font-color-green);
 
-        .social-name {
-          margin-left: 1rem;
-          font-size: 1.4rem;
-          font-weight: 300;
-          color: var(--font-color-gray);
+
+          .social-name {
+            margin-left: 1rem;
+            font-size: 1.4rem;
+            font-weight: 300;
+            color: var(--font-color-gray);
+          }
+        }
+      }
+    }
+  }
+}
+@media (max-width: 1500px) {
+  .header-element {
+    .header-content {
+      width: 80%;
+      .title {
+        font-size: 1.8rem;
+      }
+      .header-social-list {
+
+      .social-icon {
+          a {
+            .social-name {
+              font-size: 1.2rem;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+@media (max-width: 1300px) {
+  .header-element {
+    .header-content {
+      width: 90%;
+    }
+  }
+}
+@media (max-width: 800px) {
+  .header-element {
+    .header-content {
+      width: 70%;
+    }
+  }
+}
+@media (max-width: 600px) {
+  .header-element {
+    .header-content {
+      width: 90%;
+    }
+  }
+}
+@media (max-width: 450px) {
+  .header-element {
+    .header-content {
+      .header-social-list {
+        .social-icon {
+          .social-name {
+            display: none;
+          }
         }
       }
     }
