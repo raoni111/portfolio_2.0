e6139c3cef4fcb713c0745eaebcb8a3c410fed31
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2025-04-09 09:03:13 -0300

    add-img

---
+++ src/components/utils/experience-year.ts
@@ -0,0 +1,8 @@
+const getExperienceYear = () => {
+  const date = new Date();
+
+  return `${date.getFullYear() - 2022 - 1}-${(date.getFullYear() - 2022 - 1) + 1}`
+}
+
+
+export default getExperienceYear
